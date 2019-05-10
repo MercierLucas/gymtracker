f9cb7af70566d09f63732b14d48145cc0343befa
MercierLucas <lucas.mercier@isep.fr>
2019-05-10 08:03:29 +0200

    del exercices(2)

--- Controller/behaviour.js
+++ Controller/behaviour.js
@@ -26,11 +26,26 @@ var getHttpRequest= function(){
     return httpRequest;
 }
 
-function sendAjaxSelect(httpRequest){
+function ajaxSelect(){
+  var httpRequest=getHttpRequest();
   httpRequest.open('POST','./Model/getExerciceList.php',true);
   var data=new FormData();
   data.append('name',document.getElementById('mslgroup').value.toLowerCase());
-  httpRequest.send(data); 
+  httpRequest.send(data);
+  console.log("recherche sur: "+document.getElementById('mslgroup').value.toLowerCase());
+  httpRequest.onreadystatechange=function(){
+    if(httpRequest.readyState===4){
+        //document.getElementById('result').innerHTML=httpRequest.responseText;
+        console.log('Input: '+httpRequest.responseText);
+        obj=JSON.parse(httpRequest.responseText);
+        console.log(obj);
+        removeChild('exgroup'); // efface les anciennes données
+        if(obj.length==0) $('#exgroup').append("<option disabled>No exercices found</option>");
+        for(var i=0;i<obj.length;i++){
+          $('#exgroup').append("<option value='"+obj[i].name+"'>"+obj[i].name+"</option>");
+        }     
+    }
+} 
 }
 
 function ajaxExinprogram(id){
@@ -57,28 +72,13 @@ function ajaxExinprogram(id){
 $('#mslgroup').change(function(e){
     console.log('changed');
     e.preventDefault();
+    ajaxSelect()
     //var form=document.getElementById('form');
-    var httpRequest=getHttpRequest();
-    sendAjaxSelect(httpRequest);
-    console.log("recherche sur: "+document.getElementById('mslgroup').value.toLowerCase());
-    httpRequest.onreadystatechange=function(){
-      if(httpRequest.readyState===4){
-          //document.getElementById('result').innerHTML=httpRequest.responseText;
-          console.log('Input: '+httpRequest.responseText);
-          obj=JSON.parse(httpRequest.responseText);
-          console.log(obj);
-          removeChild('exgroup'); // efface les anciennes données
-          if(obj.length==0) $('#exgroup').append("<option disabled>No exercices found</option>");
-          for(var i=0;i<obj.length;i++){
-            $('#exgroup').append("<option value='"+obj[i].name+"'>"+obj[i].name+"</option>");
-          }     
-      }
-  }
+
 });  
 
 window.addEventListener("load", function(e) {
-  var httpRequest=getHttpRequest();
-  sendAjaxSelect(httpRequest);
+  ajaxSelect();
 });
 
 $('.program').click(function(e) {
